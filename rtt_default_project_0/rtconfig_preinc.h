64defdc57b3a73ac363be459b61c9ce22dce8804
thread-liu <lk9608@outlook.com>
2020-11-05 15:26:08 +0800

    [update] first commit.

---
+++ rtt_default_project_0/rtconfig_preinc.h
@@ -0,0 +1,19 @@
+
+#ifndef RTCONFIG_PREINC_H__
+#define RTCONFIG_PREINC_H__
+
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread pre-include file */
+
+#define CORE_CM4
+#define HAVE_CCONFIG_H
+#define METAL_INTERNAL
+#define METAL_MAX_DEVICE_REGIONS 2
+#define NO_ATOMIC_64_SUPPORT
+#define RT_USING_NEWLIB
+#define STM32MP157Axx
+#define USE_HAL_DRIVER
+#define VIRTIO_SLAVE_ONLY
+#define __LOG_TRACE_IO_
+
+#endif /*RTCONFIG_PREINC_H__*/
